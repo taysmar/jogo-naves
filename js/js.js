b2521cbe6de6a2c30acd5119860eaf6cc14d07bc
Taysmar <taysmar@outlook.com>
2021-01-26 16:27:09 -0300

    Criando as teclas de movimentação

--- js/js.js
+++ js/js.js
@@ -7,4 +7,59 @@ function start() {
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo'></div>");
 
+
+	
+	var jogo = {}
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+		}
+	
+	jogo.pressionou = [];
+
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+		});
+	
+	
+		$(document).keyup(function(e){
+		   jogo.pressionou[e.which] = false;
+		});
+	
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	movejogador();
+	} 
+	function movefundo() {
+	
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		$("#fundoGame").css("background-position",esquerda-1);
+		
+		} 
+		function movejogador() {
+	
+			if (jogo.pressionou[TECLA.W]) {
+				var topo = parseInt($("#jogador").css("top"));
+				$("#jogador").css("top",topo-10);
+			
+			}
+			
+			if (jogo.pressionou[TECLA.S]) {
+				
+				var topo = parseInt($("#jogador").css("top"));
+				$("#jogador").css("top",topo+10);	
+			}
+			
+			if (jogo.pressionou[TECLA.D]) {
+				
+				//Chama função Disparo	
+			}
+		
+			} // fim da função movejogador()
+
 }
