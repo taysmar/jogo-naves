0940bfab6c211dc3ae52586d0b8f392fd16f9d40
Taysmar <taysmar@outlook.com>
2021-01-26 17:31:20 -0300

    Adicionada movimentação dos itens do jogo

--- js/js.js
+++ js/js.js
@@ -10,6 +10,8 @@ function start() {
 
 	
 	var jogo = {}
+	var velocidade=5;
+	var posicaoY = parseInt(Math.random() * 334);
 	var TECLA = {
 		W: 87,
 		S: 83,
@@ -34,6 +36,9 @@ function start() {
 	
 	movefundo();
 	movejogador();
+	moveinimigo1();
+	moveinimigo2();
+	moveamigo();
 	} 
 	function movefundo() {
 	
@@ -46,20 +51,63 @@ function start() {
 			if (jogo.pressionou[TECLA.W]) {
 				var topo = parseInt($("#jogador").css("top"));
 				$("#jogador").css("top",topo-10);
+				if (topo<=0) {
+				$("#jogador").css("top",topo+10);
+				}
 			
 			}
 			
 			if (jogo.pressionou[TECLA.S]) {
 				
 				var topo = parseInt($("#jogador").css("top"));
-				$("#jogador").css("top",topo+10);	
+				$("#jogador").css("top",topo+10);
+				if (topo>=434) {	
+					$("#jogador").css("top",topo-10);
+						
+				}
+				
+				
 			}
 			
 			if (jogo.pressionou[TECLA.D]) {
 				
-				//Chama função Disparo	
+				
 			}
 		
-			} // fim da função movejogador()
+			} 
+			function moveinimigo1() {
 
+				posicaoX = parseInt($("#inimigo1").css("left"));
+				$("#inimigo1").css("left",posicaoX-velocidade);
+				$("#inimigo1").css("top",posicaoY);
+					
+					if (posicaoX<=0) {
+					posicaoY = parseInt(Math.random() * 334);
+					$("#inimigo1").css("left",694);
+					$("#inimigo1").css("top",posicaoY);
+						
+					}
+			}
+			function moveinimigo2() {
+				posicaoX = parseInt($("#inimigo2").css("left"));
+			$("#inimigo2").css("left",posicaoX-3);
+						
+				if (posicaoX<=0) {
+					
+				$("#inimigo2").css("left",775);
+							
+				}
+		}
+		function moveamigo() {
+	
+			posicaoX = parseInt($("#amigo").css("left"));
+			$("#amigo").css("left",posicaoX+1);
+						
+				if (posicaoX>906) {
+					
+				$("#amigo").css("left",0);
+							
+				}
+		
+		}
 }
